d12ffdbe8982a0bc9b5e72031d50fa5e68d02496
William <william@wtdawson.info>
2025-06-20 14:36:27 +0100

    Cleaned up imports

--- DumpDNS/Functionality/Records/URI.cs
+++ DumpDNS/Functionality/Records/URI.cs
@@ -1,9 +1,4 @@
 ﻿using DnsClient;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DumpDNS.Functionality.Records
 {
--- DumpDNS/Functionality/Results.cs
+++ DumpDNS/Functionality/Results.cs
@@ -1,10 +1,4 @@
 ﻿using DnsClient;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection.Metadata.Ecma335;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DumpDNS.Functionality
 {
