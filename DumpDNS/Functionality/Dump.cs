cc9b5de9e78459c2f5247198981db5070a446a98
William Dawson <William@wtdawson.info>
2026-03-03 21:47:17 +0000

    feat: Added custom DNS support

--- DumpDNS/Functionality/Dump.cs
+++ DumpDNS/Functionality/Dump.cs
@@ -4,8 +4,17 @@ namespace DumpDNS.Functionality
 {
     public static class Dump
     {
-        public static IDnsQueryResponse Start(string domain, (int, int) dimensions)
+        public static LookupClient client = new();
+
+        public static IDnsQueryResponse Start(string domain, (int, int) dimensions, string? ip_str = null)
         {
+            if(ip_str != null)
+            {
+                (System.Net.IPAddress, int)? result = ParseIP(ip_str);
+                if(result != null)
+                    client = new(result.Value.Item1, result.Value.Item2);
+            }
+
             Program.ActiveInstructions = Program.BottomInstructions.ProcessingNoCancel;
             if(Program.Render != null) Program.Render(null, dimensions);
             Console.CursorTop = 2;
@@ -18,6 +27,25 @@ namespace DumpDNS.Functionality
             }
             var lookup = new LookupClient();
             return lookup.Query(domain, QueryType.ANY); // This wont work, but needs to return something
+        }
+
+        public static (System.Net.IPAddress, int)? ParseIP(string ip)
+        {
+            System.Net.IPAddress? address;
+            int port = 53;
+
+            string[] split_a = ip.Split(":");
+            if (split_a.Length == 2)
+            {
+                port = int.Parse(split_a[1]);
+            }
+            else if (split_a.Length == 0 || split_a.Length < 2)
+                return null;
+
+            if (!System.Net.IPAddress.TryParse(split_a[0], out address))
+                return null;
+
+            return (address, port);
         }
     }
 }
